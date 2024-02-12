40a22b639956de1e37ca7f38a949758add01e710
Leibniz Li <stormte@gmail.com>
2024-02-13 00:58:54 +0800

    windows setup

--- forge.config.js
+++ forge.config.js
@@ -1,22 +1,16 @@
 module.exports = {
+  electronPackagerConfig: {
+    icon: "./build/icon",
+    win32metadata:{
+      ProductName: "Panda",
+      CompanyName: "arayofsunshine.dev"
+    }
+  },
   packagerConfig: {
     // dir: "./app",
     //ignore: [".idea", ".gitignore", "^/src", "^/demo", "^/dist"],
     ignore: [".idea", ".gitignore", "^/.env$", "^/demo", "^/dist"],
     icon: "./build/icon",
-    win32: {
-      icon: "./build/icon.ico",
-      target: "nsis",
-      config: {
-        productName: "Panda",
-        exe: "Panda.exe",
-        description: "A font processing App, Support for macOS and Windows.",
-        authors: "arayofsunshine.dev",
-        noMsi: true,
-        setupExe: "Panda-Setup.exe",
-        setupIcon: "./build/icon.ico"
-      }
-    }
   },
   rebuildConfig: {},
 
